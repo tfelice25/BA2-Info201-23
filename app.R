0083023fdb412f900452fc4dc09f985b43142311
lucaschia800 <lucaschia800@gmail.com>
2023-03-03 18:55:41 -0800

    update

--- app.R
+++ app.R
@@ -9,18 +9,25 @@ bike_data <- read_delim("thalia's work/SeoulBikeData3.CSV")
 ui <- fluidPage(
   
   titlePanel("BA2 Info Final App"),
-  
-  
-
-
+  tabsetPanel(type = "tabs",
+              tabPanel("Overview Page"),
+              tabPanel("Question 1"),
+              tabPanel("Question 2"),
+              tabPanel("Question 3"),
+              tabPanel("QUestion 4"),
+              
+              
+              
+              
+  )
 )
 
 server <- function(input, output) {
   
   
-
-
-
+  
+  
+  
 }
 
 # Run the application 
