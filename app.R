ff67d1060d6c1602c8fca6baceb77be8702b85e1
lucaschia800 <lucaschia800@gmail.com>
2023-03-05 19:50:20 -0800

    graph functionality done

--- app.R
+++ app.R
@@ -17,8 +17,8 @@ ui <- fluidPage(
                        sidebarLayout(
                          sidebarPanel(checkboxGroupInput("Seasons", label="Seasons", choices = list(
                            "Year Round"="Year Round", "Spring"="Spring", "Summer"="Summer",
-                           "Autumn"="Autumn", "Winter"="Winter"), selected = "Year Round"))
-                         ,
+                           "Autumn"="Autumn", "Winter"="Winter"), selected = "Year Round")
+                           ),
                          mainPanel(plotOutput("hour_plot")),
                        )),
               tabPanel("Question 4"),
@@ -34,11 +34,28 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   output$hour_plot <- renderPlot({
-    bike_data %>% 
+    
+    year_round_data <- bike_data %>% 
       select(hour,num_bikes_rented,seasons) %>% 
       group_by(hour) %>% 
-      summarize(avg_bikes=mean(num_bikes_rented)) %>% 
-      ggplot(aes(hour,avg_bikes))+geom_point()+geom_smooth()
+      summarize(avg_bikes=mean(num_bikes_rented))
+    
+    seasons_data <- bike_data %>% 
+      select(hour,num_bikes_rented,seasons) %>% 
+      filter(seasons%in%input$Seasons) %>% 
+      group_by(hour,seasons) %>% 
+      summarize(avg_bikes=mean(num_bikes_rented))
+    
+    if ("Year Round"%in%input$Seasons==TRUE) {
+        ggplot()+geom_point(data=year_round_data,aes(hour,avg_bikes),color="Black")+geom_smooth(data=year_round_data,aes(hour,avg_bikes),se=FALSE,color="Black")+
+        geom_point(data=seasons_data,aes(hour,avg_bikes,col=seasons))+geom_smooth(data=seasons_data,aes(hour,avg_bikes,col=seasons),se=FALSE)
+      
+    }
+    else{
+      ggplot()+geom_point(data=seasons_data,aes(hour,avg_bikes,col=seasons))+geom_smooth(data=seasons_data,aes(hour,avg_bikes,col=seasons),se=FALSE)
+    }
+    
+    
   })
   
   
